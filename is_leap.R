60f0243e42d8440bff8ae7fd50aba54178a3fe11
Jade Chen <jadeeechen@gmail.com>
2025-03-17 16:58:41 -0700

    Write the function specifications and documentation - but do not implement the function

---
+++ is_leap.R
@@ -0,0 +1,26 @@
+#,  Determines whether or a given year is a leap year.
+#'
+#' A year is a leap year if:
+#' - The year is divisible by four.
+#' - But, if it is also divisible by 100, it is NOT a leap year.
+#' - However, if it is divisible by 400, it IS a leap year.
+#' 
+#' @param year An integer representing the year.
+#'
+#' @return A logical value:
+#'   - TRUE if the year is a leap year.
+#'   - FALSE if the year is not a leap year.
+#'   - An error if the input is invalid (e.g., negative, zero, non-numeric).
+#'
+#' @export
+#'
+#' @examples
+#' is_leap(1992) # TRUE
+#' is_leap(2000) # TRUE
+#' is_leap(1900) # FALSE
+#' is_leap(2021) # FALSE
+#' count_classes(mtcars, am)
+is_leap <- function(year) {
+  # Returns true/false if it is a leap year (function body will be implemented later).
+
+}
