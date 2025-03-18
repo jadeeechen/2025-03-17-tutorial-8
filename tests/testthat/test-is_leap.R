fa0e1c1ff3595e18b85e95dc4f4b222d637d4676
Jade Chen <jadeeechen@gmail.com>
2025-03-17 17:41:47 -0700

    Write expect_* statements with {testthat} for each test case

--- tests/testthat/test-is_leap.R
+++ tests/testthat/test-is_leap.R
@@ -1,14 +1,23 @@
 # Valid cases
-is_leap(1992)  # Expected: TRUE
-is_leap(1900)  # Expected: FALSE
+test_that("`is_leap` correctly identifies leap years", {
+  expect_true((is_leap(1992)))  # Expected: TRUE
+})
+
+test_that("`is_leap` correctly identifies non-leap years", {
+  expect_false(is_leap(1900))  # Expected: FALSE
+})
 
 # Edge cases
-is_leap()      # Expected: Error (missing argument)
-is_leap(0)     # Expected: Error (invalid year)
+test_that("`is_leap` handles edge cases correctly", {
+  expect_error(is_leap(), "No input. Argument \"year\" is missing.")  # Expected: Error (missing argument)
+  expect_error(is_leap(0), "Year 0 is an invalid year. Must be a positive integer.")  # Expected: Error (invalid year)
+})
 
 # Invalid input cases
-is_leap("2000")   # Expected: Error (non-numeric inpu - quotes)
-is_leap("abc")    # Expected: Error (non-numeric input - letters)
-is_leap(-400)     # Expected: Error (negative year)
-is_leap(2000.5)   # Expected: Error (decimal input)
-is_leap(NA)       # Expected: Error (missing value)
+test_that("`is_leap` throws errors for invalid input types", {
+  expect_error(is_leap("2000"), "Year must be a single numeric value, no numeric inputs as strings.")  # Expected: Error (non-numeric input - numeric as string)
+  expect_error(is_leap("abc"), "Year must be a single numeric value, no strings (letters).")   # Expected: Error (non-numeric input - letters)
+  expect_error(is_leap(-400), "Year must be a positive integer.")  # Expected: Error (negative year)
+  expect_error(is_leap(2000.5), "Year must be a single numeric value, no decimals.")  # Expected: Error (decimal input)
+  expect_error(is_leap(NA), "Year must be a single numeric value, no missing values.")      # Expected: Error (missing value)
+})
